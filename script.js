2777bb385a8eaf52d09387dd68f753cb863a3eea
Mihail-D <nuby.4register@gmail.com>
2020-03-19 13:01:01 +0300

    fix: styling improvement

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 
 const MENU = document.getElementById("main-nav");
 
-MENU.addEventListener("click", event => {
+MENU.addEventListener("click",event =>
+{
 	MENU.querySelectorAll("a").forEach(el =>
 		el.classList.remove("main-nav__link--active")
 	);
@@ -17,14 +18,18 @@ let sliderButtonLeft = document.querySelector(".slider-button--left");
 let sliderButtonRight = document.querySelector(".slider-button--right");
 let sliderItemsSet = document.querySelectorAll(".slider-image__wrapper");
 
-sliderButtonLeft.addEventListener("click", function () {
-	for (let i = 0; i < sliderItemsSet.length; i++) {
+sliderButtonLeft.addEventListener("click",function ()
+{
+	for (let i = 0; i < sliderItemsSet.length; i++)
+	{
 		sliderItemsSet[i].classList.toggle("visually-hidden");
 	}
 });
 
-sliderButtonRight.addEventListener("click", function () {
-	for (let i = 0; i < sliderItemsSet.length; i++) {
+sliderButtonRight.addEventListener("click",function ()
+{
+	for (let i = 0; i < sliderItemsSet.length; i++)
+	{
 		sliderItemsSet[i].classList.toggle("visually-hidden");
 	}
 });
@@ -40,19 +45,23 @@ let sliderPhoneScreenTwo_Off = document.querySelector(
 	".slider__screen-off--horizontal"
 );
 
-sliderPhoneScreenOne.addEventListener("click", function () {
+sliderPhoneScreenOne.addEventListener("click",function ()
+{
 	sliderPhoneScreenOne_Off.classList.toggle("visually-hidden");
 });
 
-sliderPhoneScreenOne_Off.addEventListener("click", function () {
+sliderPhoneScreenOne_Off.addEventListener("click",function ()
+{
 	sliderPhoneScreenOne_Off.classList.add("visually-hidden");
 });
 
-sliderPhoneScreenTwo.addEventListener("click", function () {
+sliderPhoneScreenTwo.addEventListener("click",function ()
+{
 	sliderPhoneScreenTwo_Off.classList.toggle("visually-hidden");
 });
 
-sliderPhoneScreenTwo_Off.addEventListener("click", function () {
+sliderPhoneScreenTwo_Off.addEventListener("click",function ()
+{
 	sliderPhoneScreenTwo_Off.classList.add("visually-hidden");
 });
 
@@ -60,7 +69,8 @@ sliderPhoneScreenTwo_Off.addEventListener("click", function () {
 
 let galleryMenu = document.querySelector(".portfolio-nav");
 
-galleryMenu.addEventListener("click", event => {
+galleryMenu.addEventListener("click",event =>
+{
 	event.preventDefault();
 	galleryMenu
 		.querySelectorAll(".portfolio-nav__link")
@@ -74,18 +84,25 @@ galleryMenu.addEventListener("click", event => {
 let gallery = document.querySelector(".gallery");
 let galleryImageSet = gallery.querySelectorAll(".gallery img");
 
-gallery.addEventListener("click", function () {
+gallery.addEventListener("click",function ()
+{
 	let target = event.target;
-	for (let i of galleryImageSet) {
-		(i !== target) ? i.classList.remove("gallery__image"): event.target.classList.toggle("gallery__image");
+	for (let i of galleryImageSet)
+	{
+		i !== target ?
+			i.classList.remove("gallery__image") :
+			event.target.classList.toggle("gallery__image");
 	}
 });
 
 // перемешивание картинок в галлерее
 
-galleryMenu.addEventListener("click", function () {
-	for (var i = gallery.children.length; i >= 0; i--) {
-		if (event.target.classList.contains("portfolio-nav__link")) {
+galleryMenu.addEventListener("click",function ()
+{
+	for (var i = gallery.children.length; i >= 0; i--)
+	{
+		if (event.target.classList.contains("portfolio-nav__link"))
+		{
 			gallery.appendChild(gallery.children[(Math.random() * i) | 0]);
 		}
 	}
@@ -98,28 +115,35 @@ let modalSubmitButton = submitForm.querySelector(
 	".modal-window__submit-button"
 );
 
-let subjectField = document.querySelector(
-	".communication-block__input--subject"
-);
-let describeField = document.querySelector(
-	".communication-block__text-area"
-);
+let subjectField = document.querySelector(".communication-block__input--subject");
+let describeField = document.querySelector(".communication-block__text-area");
+let nameField = document.querySelector(".communication-block__input--name");
+let mailField = document.querySelector(".communication-block__input--mail");
 
 let modalSubject = modalBlock.querySelector(".modal-window__subject");
 let modalDescribe = modalBlock.querySelector(".modal-window__describe");
 
 // появление модального окна и обработка формы
-submitForm.addEventListener("submit", function (el) {
+submitForm.addEventListener("submit",function (el)
+{
 	el.preventDefault();
 	modalBlock.classList.remove("visually-hidden");
 
-	(subjectField.value.length > 0) ? modalSubject.innerHTML = "Тема: " + subjectField.value: ("Без темы");
+	subjectField.value.length > 0 ?
+		(modalSubject.innerHTML = "Тема: " + subjectField.value) :
+		"Без темы";
 
-	(describeField.value.length > 0) ? modalDescribe.innerHTML = "Описание: " + describeField.value: "Без описания";
-
+	describeField.value.length > 0 ?
+		(modalDescribe.innerHTML = "Описание: " + describeField.value) :
+		"Без описания";
 });
 
 //закрытие модального окна
-modalSubmitButton.addEventListener("click", function () {
+modalSubmitButton.addEventListener("click",function ()
+{
+	subjectField.value = '';
+	describeField.value = '';
+	nameField.value = '';
+	mailField.value = '';
 	modalBlock.classList.add("visually-hidden");
 });
